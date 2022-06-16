f8f9fc191215c3f7ea7e4a85b1a0bf2bb2364509
Anthony Garcia <anthonygarciiia@icloud.com>
2022-06-15 19:25:37 -0500

    Primera Tarea 02-JS-I 2.0

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -260,33 +260,6 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  /*var vocales = ["a","e","i","o","u"]
-  if (letra === "a"||"e"||"i"||"o"||"u") {
-    return "Es vocal"
-  } else {
-    if (letra.length > 1) {
-      return "Dato incorrecto"
-      
-    }else { 
-      return "Dato incorrecto"
-    }
-
-  }
-  }
-  var vocales = ["a","e","i","o","u"]
-  if (letra.length > 1) {
-    return "Dato incorrecto";
-  } 
-
-  if (letra === vocales) {
-    return "Es vocal";
-  } else if (letra !== vocales) {
-    return "Dato incorrecto";
-  }
-  
-  */
-
-
 
   if (letra.length > 1) {
     return "Dato incorrecto";
